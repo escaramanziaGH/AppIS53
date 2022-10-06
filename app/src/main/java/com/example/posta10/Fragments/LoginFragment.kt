cc311d8a01e63ef269a22dd36b957e29a73355ff
escaramanziaGH <valeria.cavallo@gmail.com>
2022-10-06 19:25:08 -0300

    Initial commit

---
+++ app/src/main/java/com/example/posta10/Fragments/LoginFragment.kt
@@ -0,0 +1,26 @@
+package com.example.posta10.Fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.posta10.R
+import com.example.posta10.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    private lateinit var binding: FragmentLoginBinding // variable de inicializacion
+    
+ 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding=FragmentLoginBinding.bind(view) // Llama a la vista, reemplaza a binding=FragmentLoginBinding.inflate(layoutinflater)
+
+        binding.botonRegistrarse.setOnClickListener {
+            
+            findNavController().navigate(R.id.action_loginFragment_to_registrarseFragment)
+        }
+
+    }
+}
